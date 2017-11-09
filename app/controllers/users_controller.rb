e4a0c17a38b21b9093d3baed5d90b24f092ac7d2
github username <github email address>
2017-11-09 15:45:44 -0800

    added flash message to application layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Congrats, you're all signed up!"
       redirect_to @user
     else
       render "new"
