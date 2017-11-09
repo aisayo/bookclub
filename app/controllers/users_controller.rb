e3253179d64f22c8f506158bd3a6d19c74932e7c
github username <github email address>
2017-11-08 19:30:16 -0800

    added users show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,13 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
-  def show #individual users page
+  def show #individual users profile page
+    @user = User.find_by(id: params[:id])
   end
 
   def new #make new user/signup
+    #should render sign up form
+    #redirects to users show page
   end
 
 end
