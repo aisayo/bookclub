ef8461979bd11dfc3691e67dfb8a854ba77de4a1
github username <github email address>
2017-11-08 20:13:26 -0800

    added links to usernames

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def hello
-    render html: "I am a Homepage!"
+  def homepage
+    
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,24 @@
 class UsersController < ApplicationController
 
   def index #lists all users
+    #only signed up/logged in users can see
     @users = User.all
+  end
+
+  def new #make new user/signup
+    #should render sign up form
+    #redirects to users show page
   end
 
   def show #individual users profile page
     @user = User.find_by(id: params[:id])
   end
 
-  def new #make new user/signup
-    #should render sign up form
-    #redirects to users show page
+
+  def create
+    #submits forms
+    #validates params
+    #creates a new user
   end
 
 end
