dce6528e04d600f38d2f911d2b8f143ef83bfc70
github username <github email address>
2017-11-09 13:20:20 -0800

    added validations for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,17 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+
   has_many :books
   has_many :ratings
   has_many :categories, through: :books
+
+  validates :username, presence: true, length: { maximum: 10}
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 30},
+  format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 8 }
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :categories #nested route categories/1/books/1
     resources :books
       resources :review
+  get '/signup', to: 'users#new'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
