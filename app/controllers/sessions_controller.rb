21917bfd689dd44b157483e3cfaf26bc613560dd
github username <github email address>
2017-11-09 17:21:05 -0800

    added flash message to incorrect login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,19 @@ class SessionsController < ApplicationController
   end
 
   def create
-    render 'new'
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+       redirect_to @user
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
   end
 
   def destroy
   end
 
 end
+
+
+#flash.now is specifically designed for displaying flash messages on rendered pages
