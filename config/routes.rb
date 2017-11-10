73eb7c28843b7e86d07dc84160e0c8048cd94173
github username <github email address>
2017-11-09 16:44:50 -0800

    added login form

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
   root "application#homepage"
-  resources :categories #nested route categories/1/books/1
-    resources :books
-      resources :review
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
   resources :users
+
+
+  #resources :categories #nested route categories/1/books/1
+    #resources :books
+      #resources :review
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
