5d430b468ec5da9f5abbfef95f84c37d288e8369
github username <github email address>
2017-11-09 17:37:49 -0800

    created log_in helper

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(username: params[:session][:username])
     if user && user.authenticate(params[:session][:password])
-       redirect_to @user
+       log_in user
+       redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,9 @@
 module SessionsHelper
+
+  def log_in(user)
+    session[:id] = user.id
+    #places a temp cookie on browser
+    #contains encrypted id
+    #can be used through browser until browser closed
+  end
 end
