7a17931643a10afc9212316fd483831cbfb97b64
github username <github email address>
2017-11-09 18:39:47 -0800

    created logged_in? method

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,4 +6,13 @@ module SessionsHelper
     #contains encrypted id
     #can be used through browser until browser closed
   end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
 end
