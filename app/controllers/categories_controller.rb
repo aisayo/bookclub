b7c610ddbdef3eab40fc8cc801e9326ec314030e
github username <github email address>
2017-11-10 11:19:02 -0800

    created add new category feature

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,6 +3,13 @@ class CategoriesController < ApplicationController
     @categories = Category.all
   end
 
+  def new
+    @category = Category.new
+  end
+
+  def create
+  end
+
   def show
     @category = Category.find_by(id: params[:id])
   end
