e06008c917f4a8a6fe525af4fb8431c540df3dca
github username <github email address>
2017-11-11 20:53:30 -0800

    created find_or_Create_by_omni

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,11 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 8 }
 
+  def self.find_or_create_by_omniauth(auth_hash)
+    where(email: auth_hash[:info][:email]).first_or_create do |user|
+      user.name = auth_hash[:info][:name]
+      user.password = SecureRandom.hex
+    end
+  end
+
 end
