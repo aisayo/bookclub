1ba23cf89613af362d11666f4d0ccf2a81448e11
github username <github email address>
2017-11-27 12:05:07 -0800

    created custom writer

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,7 +3,11 @@ class Book < ApplicationRecord
   has_many :reviews
   has_many :users, through: :reviews
 
-  accepts_nested_attributes_for :reviews
+  def reviews_attributes=(reviews_attributes)
+    reviews_attributes.values.each do |reviews_attributes|
+      self.reviews.build(reviews_attributes)
+    end
+  end
 
   validates :title, :author, presence: true
   validates :title, uniqueness: true
