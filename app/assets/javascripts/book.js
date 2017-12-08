f65aa983f31f3f8503f2b2c18195476c881e5a37
github username <github email address>
2017-12-08 14:15:31 -0800

    added render json to Review controller

--- app/assets/javascripts/book.js
+++ app/assets/javascripts/book.js
@@ -1,6 +1,11 @@
 $(document).on('turbolinks:load', function () {
     $("a.reviews_link").on("click", function(e){
-    alert("you clicked!!!")
+    $.ajax({
+      method: "GET",
+      url: "this.href"
+    }).done(function(data){
+      console.log(data)
+    })
     e.preventDefault();
   })
 })
