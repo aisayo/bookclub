2b3154f1e22249c95fc92c2b868775f887e68522
github username <github email address>
2017-12-08 16:40:00 -0800

    creating click event for reviews link in book.js

--- app/assets/javascripts/book.js
+++ app/assets/javascripts/book.js
@@ -1,11 +1,13 @@
 $(document).on('turbolinks:load', function () {
     $("a.reviews_link").on("click", function(e){
-    $.ajax({
-      method: "GET",
-      url: "this.href"
-    }).done(function(data){
-      console.log(data)
-    })
-    e.preventDefault();
+      console.log(this.href)
+      $.get(this.href).success(function(response){
+        console.log(response)
+      })
+      e.preventDefault();
   })
 })
+
+
+
+// e.preventDefault();
