c55f55bc437ac8fb27aaf5ec22a066ab08b6ecaf
github username <github email address>
2017-12-08 21:51:31 -0800

    refractored function in book.js into loadReviews()

--- app/assets/javascripts/book.js
+++ app/assets/javascripts/book.js
@@ -1,20 +1,24 @@
 $(document).on('turbolinks:load', function () {
-    $("a.reviews_link").on("click", function(e){
-      $("a.reviews_link").hide();
-      $.get(this.href).success(function(reviews){
-        $.each(reviews, function(index, review){
-          $("div.reviews").append(
-            "<li>" +
-            '<b>' + "Review Title:" + "</b>" + " " + review.title + " " + "<br>" +
-            '<b>' + "Content:" + "</b>" + " " + review.content + " " + "<br>" +
-            '<b>' + "Rating:" + "</b>" + " " + review.rating + " " +
-            "</li>"
-          )
-        })
-      })
-      e.preventDefault();
-  })
+  loadReviews();
 })
+
+function loadReviews(){
+  $("a.reviews_link").on("click", function(e){
+    $("a.reviews_link").hide();
+    $.get(this.href).success(function(reviews){
+      $.each(reviews, function(index, review){
+        $("div.reviews").append(
+          "<li>" +
+          '<b>' + "Review Title:" + "</b>" + " " + review.title + " " + "<br>" +
+          '<b>' + "Content:" + "</b>" + " " + review.content + " " + "<br>" +
+          '<b>' + "Rating:" + "</b>" + " " + review.rating + " " +
+          "</li>"
+        )
+      })
+    })
+    e.preventDefault();
+})
+}
 
 
 
