eb9829ddbf89b80ef9424378cc610fc4ba050fb9
github username <github email address>
2017-12-13 08:53:22 -0800

    added an interpolated URL to leaveReviews function

--- app/assets/javascripts/book.js
+++ app/assets/javascripts/book.js
@@ -1,10 +1,18 @@
 // this sets the index at 0, but how do i get it to start at 1?
-var index = 0
+let index = 0
 
 $(document).ready(function () {
   loadReviews();
   leaveReview();
 })
+
+class Review{
+  constructor(title, content, rating){ //do I need to construct user_id and ID for recipe?
+    this.title = title
+    this.content = content
+    this.rating = rating
+  }
+}
 
 function loadReviews(){
   $("a.reviews_link").on("click", function(e){
@@ -26,30 +34,17 @@ function loadReviews(){
 })
 };
 
-// function leaveReview(){
-//   $("#new_review").on("submit", function(e){
-//     $.ajax({
-//       type: ($("input[name='method']").val() || this.method),
-//       url: this.action,
-//       data: this.serialize(),
-//       success: function(response){
-//         $("div.reviews").append(response)
-//       }
-//     })
-//     e.preventDefault();
-//   })
-// }
-
 function leaveReview(form){
   $("#new_review").on("submit", function(e) {
-    var bookId = parseInt($("#review_book_id").attr("value"));
-    postUrl = "/books/" + bookId + "/reviews";
+    const bookId = parseInt($("#review_book_id").attr("value"));
+    postUrl =`/books/${bookId}/reviews`; //use interpolation, concate
       $.ajax({
           url: postUrl,
-          data: $(form).serialize(),
+          data: $(this).serialize(),
           type: "POST",
           dataType: "json",
           success: function(response) {
+            new Review(title, content, rating)
               $("div.reviews").append(response)
           },
           error: function(xhr, textStatus, errorThrown) {}
